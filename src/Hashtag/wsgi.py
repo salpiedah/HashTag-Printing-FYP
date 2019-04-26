0ee4009a67207b0dc90b83a8d965303f9e114f16
Firdaus Omar <salpiedah@gmail.com>
2019-04-27 00:27:50 +0800

    Docker setup

--- src/Hashtag/wsgi.py
+++ src/Hashtag/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Hashtag project.
+WSGI config for HashTag project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hashtag.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HashTag.settings')
 
 application = get_wsgi_application()
